0471ba0be5e4ef0021cb872402d8414b46c5bed6
theMorshed <themorshedctg@gmail.com>
2023-08-29 13:21:11 +0600

    fetch all product from database and display all products in frontend

--- store/views.py
+++ store/views.py
@@ -1,5 +1,10 @@
 from django.shortcuts import render
+from .models import Product
 
 # Create your views here.
 def home(request):
-    return render(request, 'index.html')
+    products = Product.objects.filter(is_available=True)
+    context = {
+        'products': products
+    }
+    return render(request, 'store/store.html', context)
