8a5c347fd410b0d525ffbb3ee66e68320bda4881
theMorshed <themorshedctg@gmail.com>
2023-08-30 12:21:32 +0600

    create cart and cartitem class for working with cart functionality

--- cart/models.py
+++ cart/models.py
@@ -1,3 +1,22 @@
 from django.db import models
+from store.models import Product
 
 # Create your models here.
+class Cart(models.Model):
+    cart_id = models.CharField(max_length=255, blank=True)
+    date_added = models.DateTimeField(auto_now_add=True)
+    
+    def __str__(self):
+        return self.cart_id
+    
+class CartItem(models.Model):
+    cart = models.ForeignKey(Cart, on_delete=models.CASCADE)
+    product = models.OneToOneField(Product, on_delete=models.CASCADE)
+    quantity = models.IntegerField()
+    is_active = models.BooleanField(default=True)
+    
+    def sub_total(self):
+        return self.product.price * self.quantity
+    
+    def __str__(self):
+        return self.product.product_name
