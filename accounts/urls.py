08c4b928c1d9af65b9f1f6bcef6e09a5c28b67b5
theMorshed <themorshedctg@gmail.com>
2023-08-31 13:24:32 +0600

    create profile page, create login, logout functionality with redirect expected page after login and logout

--- accounts/urls.py
+++ accounts/urls.py
@@ -1,7 +1,9 @@
 from django.urls import path
-from accounts.views import register, user_login
+from accounts.views import register, user_login, display_profile, user_logout
 
 urlpatterns = [
     path('register/', register, name='register'),
     path('login/', user_login, name='login'),
+    path('profile/', display_profile, name='profile'),
+    path('logout/', user_logout, name='logout'),
 ]
--- accounts/views.py
+++ accounts/views.py
@@ -1,12 +1,14 @@
 from django.shortcuts import render, redirect
 from accounts.forms import RegistrationForm
+from django.contrib.auth import login, logout, authenticate
 
 # Create your views here.
 def register(request):
     if request.method == 'POST':
         form = RegistrationForm(request.POST)
         if form.is_valid():
-            form.save()
+            user = form.save()
+            login(request, user)
             return redirect('store')
     else:
         form = RegistrationForm()
@@ -14,4 +16,18 @@ def register(request):
     return render(request, 'accounts/register.html', {'form': form})
 
 def user_login(request):
+    if request.method == 'POST':
+        username = request.POST.get('username')
+        password = request.POST.get('password')
+        user = authenticate(username = username, password = password)
+        login(request, user)
+        return redirect('profile')
+    
     return render(request, 'accounts/login.html')
+
+def display_profile(request):
+    return render(request, 'accounts/profile.html')
+
+def user_logout(request):
+    logout(request)
+    return redirect('login')
