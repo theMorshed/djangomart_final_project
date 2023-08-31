fb673630dcbfd6f0124128e6a7ecfeba1c9c2d9f
theMorshed <themorshedctg@gmail.com>
2023-09-01 00:31:45 +0600

    implete cart functionality

--- cart/models.py
+++ cart/models.py
@@ -1,17 +1,19 @@
 from django.db import models
 from store.models import Product
+from django.contrib.auth.models import User
 
 # Create your models here.
 class Cart(models.Model):
-    cart_id = models.CharField(max_length=255, blank=True)
+    cart_id = models.CharField(max_length=255, null=True, blank=True)
     date_added = models.DateTimeField(auto_now_add=True)
     
     def __str__(self):
         return self.cart_id
     
 class CartItem(models.Model):
-    cart = models.ForeignKey(Cart, on_delete=models.CASCADE)
-    product = models.OneToOneField(Product, on_delete=models.CASCADE)
+    user = models.ForeignKey(User, on_delete=models.CASCADE, null=True)
+    cart = models.ForeignKey(Cart, on_delete=models.CASCADE, null=True)
+    product = models.ForeignKey(Product, on_delete=models.CASCADE)
     quantity = models.IntegerField()
     is_active = models.BooleanField(default=True)
     
--- cart/views.py
+++ cart/views.py
@@ -3,49 +3,75 @@ from store.models import Product
 from cart.models import Cart, CartItem
 
 # Create your views here.
-def cart(request):
+def get_session_id(request):
     session_id = request.session.session_key
-    cartid = Cart.objects.get(cart_id=session_id)
-    cart_id = Cart.objects.filter(cart_id=session_id).exists()
+    if not session_id:
+        session_id = request.session.create()
+    return session_id
+    
+def cart(request):
     cart_items = None
     total = 0
     tax = 0
     grand_total = 0
-    if cart_id:
-        cart_items = CartItem.objects.filter(cart = cartid)
+    if request.user.is_authenticated:
+        cart_items = CartItem.objects.filter(user = request.user)
         for item in cart_items:
             total += item.sub_total()
-    tax = (total * 2) / 100
-    grand_total = total + tax
+    else:
+        session_id = get_session_id(request)
+        cartid = Cart.objects.get(cart_id=session_id)
+        cart_id = Cart.objects.filter(cart_id=session_id).exists()
+        
+        if cart_id:
+            cart_items = CartItem.objects.filter(cart = cartid)
+            for item in cart_items:
+                total += item.sub_total()
+        tax = (total * 2) / 100
+        grand_total = total + tax
+        
     context = {'cart_items': cart_items, 'total': total, 'tax': tax, 'grand_total': grand_total}
     return render(request, 'cart/cart.html', context)
 
 def add_to_cart(request, product_id):
     product = Product.objects.get(id=product_id)
-    if not request.session.session_key:
-        request.session.create()
-        
-    session_id = request.session.session_key
-    cart_id = Cart.objects.filter(cart_id=session_id).exists()
-    if cart_id:
-        cart_item = CartItem.objects.filter(product=product).exists()
+    session_id = get_session_id(request)
+    if request.user.is_authenticated:
+        cart_item = CartItem.objects.filter(product=product, user = request.user).exists()
         if cart_item:
             item = CartItem.objects.get(product=product)
             item.quantity += 1
             item.save()
         else:
-            cart_id = Cart.objects.get(cart_id = session_id)
+            # cart_id = Cart.objects.get(cart_id = session_id)
             item = CartItem.objects.create(
                 product = product,
-                cart = cart_id,
-                quantity = 1
+                # cart = cart_id,
+                quantity = 1,
+                user = request.user
             )
             item.save()
     else:
-        cart_id = Cart.objects.create(
-            cart_id = session_id
-        )
-        cart_id.save()
+        cart_id = Cart.objects.filter(cart_id=session_id).exists()
+        if cart_id:
+            cart_item = CartItem.objects.filter(product=product).exists()
+            if cart_item:
+                item = CartItem.objects.get(product=product)
+                item.quantity += 1
+                item.save()
+            else:
+                cart_id = Cart.objects.get(cart_id = session_id)
+                item = CartItem.objects.create(
+                    product = product,
+                    cart = cart_id,
+                    quantity = 1
+                )
+                item.save()
+        else:
+            cart_id = Cart.objects.create(
+                cart_id = session_id
+            )
+            cart_id.save()
     
     return redirect('cart')
 
