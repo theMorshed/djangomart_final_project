f01e2a86a29361c927e2b80a8a89caeea3113491
theMorshed <themorshedctg@gmail.com>
2023-08-31 10:14:21 +0600

    user registration form created and functionally build

--- accounts/views.py
+++ accounts/views.py
@@ -1,8 +1,17 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from accounts.forms import RegistrationForm
 
 # Create your views here.
 def register(request):
-    return render(request, 'accounts/register.html')
+    if request.method == 'POST':
+        form = RegistrationForm(request.POST)
+        if form.is_valid():
+            form.save()
+            return redirect('store')
+    else:
+        form = RegistrationForm()
+              
+    return render(request, 'accounts/register.html', {'form': form})
 
 def user_login(request):
     return render(request, 'accounts/login.html')
